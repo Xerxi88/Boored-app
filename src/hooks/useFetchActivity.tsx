6c83b9fa25f2f5675ce77837f4710d835ccb20df
xerxi88 <serjicu88@gmail.com>
2024-09-29 21:15:13 +0200

    Fixed CORS

--- src/hooks/useFetchActivity.tsx
+++ src/hooks/useFetchActivity.tsx
@@ -4,7 +4,7 @@ const useFetchActivity = (filter: string) => {
   const [activity, setActivity] = useState<string>("No hi ha activitat");
 
   const fetchActivity = () => {
-    fetch(`https://bored-api.appbrewery.com/filter?type=${filter}`)
+    fetch(`/api/filter?type=${filter}`)
       .then((res) => res.json())
       .then((data) => {
         const randomNumber = Math.floor(Math.random() * data.length);
